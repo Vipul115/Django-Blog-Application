979fca811b8cca61c1ed264f2294f2bae92c216d
vipul115 <vipulrustagi11@gmail.com>
2018-08-10 16:12:29 +0530

    Implementing mdoel forms

--- posts/urls.py
+++ posts/urls.py
@@ -5,8 +5,8 @@ from .views import post_detail,post_update,post_list,post_delete,post_create
 urlpatterns = [
     url(r'^$', post_list),
     url(r'^(?P<id>\d+)/$', post_detail, name='detail'),
-    url(r"^create/$", post_create),
-    url(r'^update/$', post_update),
+    url(r"^create/$", post_create, name='create'),
+    url(r'^update/(?P<id>\d+)/$', post_update),
     url(r'^delete/$', post_delete)
 
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,9 +1,10 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render,get_object_or_404
+from django.contrib import messages
 
 # Create your views here.
 from .models import Post
-
+from .forms import PostForm
 
 def post_list(request):
 
@@ -18,11 +19,29 @@ def post_list(request):
 
     return render(request, "index.html", context)
 
-def post_update(request):
-    return HttpResponse("<h1>Update</h1")
+def post_update(request,id):
+    instance = get_object_or_404(Post, id=id)
+    form = PostForm(request.POST or None, instance = instance)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save()
+
+        return HttpResponseRedirect(instance.get_absolute_url())
+    context = {"title":instance.title,
+                "instance" : instance,
+                "form":form
+               }
+    return render(request, "post_form.html", context)
 
 def post_create(request):
-    return HttpResponse("<h1>Create</h1")
+
+    form = PostForm(request.POST or None)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save()
+    context = {"form": form}
+    return render(request, "post_form.html", context)
+
 
 def post_detail(request, id):
     instance = get_object_or_404(Post, id=id)
